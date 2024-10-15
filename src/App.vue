b239e13f2da5d32f54372d0ccc9e2611262049bc
Jose Luis Hernandez De La Cruz <dareckluis@gmail.com>
2024-10-15 11:12:15 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <TextoFormulario />
+  </div>
+</template>
+
+<script>
+import TextoFormulario from './components/TextoFormulario.vue';
+
+export default {
+  components: {
+    TextoFormulario
+  }
+};
+</script>
+
+<style>
+/* Estilos CSS para tu aplicación principal */
+</style>
